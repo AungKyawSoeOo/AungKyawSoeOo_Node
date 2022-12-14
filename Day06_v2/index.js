dd9d4dcd2d028549b8cdae31cd07c7a6fe9ad4be
AungKyawSoeOo <iamaprogrammer123@gamil.com>
2022-12-14 18:30:42 +0630

    write Added

--- Day06_v2/index.js
+++ Day06_v2/index.js
@@ -7,28 +7,33 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // sending json array to / route (get method)
 app.get("/", (req, res) => {
-  fs.readFile(__dirname+"/user.json","utf-8", (err, data) => {
-    if (err) throw err;
-    const userArray = JSON.parse(data);
-    res.send(userArray);
-  });
+    fs.readFile(__dirname + "/user.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        const userArray = JSON.parse(data);
+        res.send(userArray);
+    });
 });
 
 // reading and add new json object to / route (post method)
 app.post("/", (req, res) => {
-  const userBody = req.body;
-  fs.readFile(__dirname+"/user.json","utf-8", (err, data) => {
-    if (err) throw err;
-    const uArray = JSON.parse(data);
-    uArray.push(userBody);
-    //const newlist = JSON.stringify(uArray);
-    res.send(uArray);
-  });
- 
- 
+    const userBody = req.body;
+    fs.readFile(__dirname + "/user.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        const uArray = JSON.parse(data);
+        uArray.push(userBody);
+        //const newlist = JSON.stringify(uArray);
+        res.send(uArray);
+        fs.writeFile(__dirname + "/user.json", JSON.stringify(uArray), err => {
+            if (err) throw err;
+            console.log("File written successfully");
+        })
+
+    });
+
+
 });
 
 
 app.listen(3000, () => {
-  console.log("Server listening at port 3000");
+    console.log("Server listening at port 3000");
 })
